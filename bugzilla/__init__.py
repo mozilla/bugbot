108ae124e96798f8eec82f9e9b22d5fe8f15d9c8
Christian Legnitto <clegnitto@mozilla.com>
2010-11-30 11:42:11 -0800

    Set default encoding to utf-8. Probbably not the best to do this for every user of the lib, but bz data can be unicode and I don't want to copy this into every helper script

--- bugzilla/__init__.py
+++ bugzilla/__init__.py
@@ -3,6 +3,10 @@ __all__ = ['models','utils','agents']
 import httplib
 from remoteobjects import http
 
+# Printing throws an error if we are printing using ascii
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Monkey patch remoteobjects to accept 202 status codes.
 http.HttpObject.response_has_content[httplib.ACCEPTED] = False
