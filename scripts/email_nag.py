1ef3098440e9a095ebb9fc5e2123f0c8d9581f7a
Lukas Blakk <lukasblakk@gmail.com>
2012-04-06 11:02:16 -0700

    Emails created for multiple queries now, but sending/manual list broken temporarily

--- scripts/email_nag.py
+++ scripts/email_nag.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-"""%prog [-d|--dryrun] [-u|--username `username`] [-p|--password `password`]
-        [-V| --version `version`]
-        [-e|--email-cc-list `email@address.com`]
-        [--verbose]
-
+"""
+A script for automated nagging emails listing all the bugs being tracked by certain queries
+These can be collated into several 'channels' through the use of multiple query files with 
+a 'channel' param set eg: 'beta', 'aurora'
+Once the bugs have been collected from bugzilla they are sorted into buckets by assignee manager
+and an email can be sent out to the assignees, cc'ing the manager about which bugs are being tracked
+for each channel
 """
 import sys, os
 import json
@@ -25,40 +27,45 @@ people = phonebook.PhonebookDirectory()
 # DONE - pull a variety of queries from passed files
 
 def get_last_assignee_comment(comments, person):
+    # go through in reverse order to get most recent
     for comment in comments[::-1]:
         if person != None:
             if comment.creator.name == person['mozillaMail'] or comment.creator.name == person['bugzillaEmail']:
-                print "Returning %s's last comment on their bug." % comment.creator.real_name
+                print "Found last assignee (%s) comment on bug. %s" % (comment.creator.real_name, comment.creation_time.replace(tzinfo=None))
                 return comment.creation_time.replace(tzinfo=None)
     return None
 
-def createEmail(manager_email, bugs, version, cc_list=None):
+def createEmail(manager_email, channels, cc_list=None):
     if cc_list == None:
         cc_list = [manager_email, FROM_EMAIL]
     toaddrs = []
 
     message_body ='''
-We're currently getting in touch with teams that have unfixed bugs tracked for Firefox %s assigned to them. 
+We're currently getting in touch with the assignees and manager of unfixed bugs we are tracking. 
 
-Here's your list:\n
-''' % version
-
-    for bug in bugs:
-        message_body += '%s -- assigned to: %s -- Last commented on: %s\n' % (bug, bug.assigned_to.real_name, bug.comments[-1].creation_time.replace(tzinfo=None))
-        if bug.assigned_to.name != 'general@js.bugs':
-            # we will email people at their LDAP email, not bugmail
-            person = dict(people.people_by_bzmail[bug.assigned_to.name])
-            if person['mozillaMail'] not in toaddrs:
-                toaddrs.append(person['mozillaMail'])
+Here's your list:
+'''
+    print channels
+    for channel,info in channels.items():
+        # TODO sort by priority flag, if exists
+        # TODO take out dupe bugs from lower priority
+        message_body += '\nBugs tracked for: %s\n---------------------------\n\n' % channel.title()
+        for bug in info['bugs']:
+            message_body += '  %s -- assigned to: %s -- Last commented on: %s\n' % (bug, bug.assigned_to.real_name, bug.comments[-1].creation_time.replace(tzinfo=None))
+            if bug.assigned_to.name != 'general@js.bugs':
+                # we will email people at their LDAP email, not bugmail
+                person = dict(people.people_by_bzmail[bug.assigned_to.name])
+                if person['mozillaMail'] not in toaddrs:
+                    toaddrs.append(person['mozillaMail'])
 
     message_body +='''
-Please either make the case for untracking, let us know what is blocking the 
-investigation, or make sure the above issues are prioritized for release. Thanks!
+Please either make the case for untracking, let us know what is blocking the investigation, 
+or make sure the above issues are prioritized for release. Thanks!
 
 Sincerely,
 Release Management Team'''
 
-    message_subject = 'Bugs Tracked for Firefox %s' % version
+    message_subject = 'Automatic Tracked Bugs Reminder'
     message = ("From: %s\r\n" % FROM_EMAIL
         + "To: %s\r\n" % ",".join(toaddrs)
         + "CC: %s\r\n" % ",".join(cc_list)
@@ -83,7 +90,6 @@ if __name__ == '__main__':
         dryrun=False,
         username=None,
         password=None,
-        version=None,
         template=None,
         wiki=False,
         email_cc_list=['release-mgmt@mozilla.com'],
@@ -95,8 +101,6 @@ if __name__ == '__main__':
             help="just do the query, and print emails to console without emailing anyone")
     parser.add_argument("-u", "--username", dest="username",
             help="specify a specific username for bugzilla")
-    parser.add_argument("-V", "--version", dest="version",
-            help="firefox version string for tracking", required=True)
     parser.add_argument("-t", "--template", dest="template",
             help="jinja template to use for the email or wiki page to create")
     parser.add_argument("--wiki", dest="wiki", action="store_true",
@@ -150,97 +154,113 @@ if __name__ == '__main__':
     for channel in collected_channels.keys():
         total_bugs += len(collected_channels[channel]['buglist'])
     print "Found %s bugs total." % total_bugs
-    print collected_channels.keys()
-    sys.exit(0)
-    
+    print "Channels to collect: %s" % collected_channels.keys()
+
     managers = people.managers
     manual_notify = []
     counter = 0
 
-    def add_to_managers(manager_email):
+    def add_to_managers(manager_email, channel):
         if managers[manager_email].has_key('nagging'):
-            managers[manager_email]['nagging'].append(bug)
+            if managers[manager_email]['nagging'].has_key(channel):
+                managers[manager_email]['nagging'][channel]['bugs'].append(bug)
+                if options.verbose:
+                    print "Adding %s to %s in nagging for %s" % (bug.id, channel, manager_email)
+            else:
+                managers[manager_email]['nagging'][channel] = { 'bugs': [bug] }
+                if options.verbose:
+                    print "Adding new channel %s for bug %s in nagging and %s" % (channel, bug.id, manager_email)
         else:
-            managers[manager_email]['nagging'] = [bug]
+            managers[manager_email]['nagging'] = {
+                    channel : { 'bugs': [bug] },
+                }
+            if options.verbose:
+                print "Creating channel %s for bug %s in nagging and %s" % (channel, bug.id, manager_email)
     
     # TODO - now go through each channel and build the notifications, then come back for template selection & creation of output (email/wiki)
-    for b in buglist:
-        counter = counter + 1
-        send_mail = True
-        bug = bmo.get_bug(b.id)
-        manual_notify.append(bug)
-        assignee = bug.assigned_to.name
-        if people.people_by_bzmail.has_key(assignee):
-            person = dict(people.people_by_bzmail[assignee])
-        else:
-            person = None
-        
-        # kick bug out if days since comment check is on
-        if options.days_since_comment != -1:
-            # try to get last_comment by assignee
-            if person != None:
-                last_comment = get_last_assignee_comment(bug.comments, person)
-            # otherwise just get the last comment
+    for channel in collected_channels.keys():
+        for b in collected_channels[channel]['buglist']:
+            counter = counter + 1
+            send_mail = True
+            bug = bmo.get_bug(b.id)
+            manual_notify.append(bug)
+            assignee = bug.assigned_to.name
+            if people.people_by_bzmail.has_key(assignee):
+                person = dict(people.people_by_bzmail[assignee])
             else:
-                last_comment = bug.comments[-1].creation_time.replace(tzinfo=None)
-            if last_comment != None:
-                print bug.comments[-1].creator.name, bug.comments[-1].creator.real_name
-                timedelta = datetime.now() - last_comment
-                if timedelta.days <= int(options.days_since_comment):
-                    if options.verbose:
-                        print "Skipping bug %s since it's had a comment within the past %s days" % (bug.id, options.days_since_comment)
-                    send_mail = False
-                    counter = counter - 1
-                    manual_notify.remove(bug)
+                person = None
+            
+            # kick bug out if days since comment check is on
+            if options.days_since_comment != -1:
+                # try to get last_comment by assignee
+                if person != None:
+                    last_comment = get_last_assignee_comment(bug.comments, person)
+                # otherwise just get the last comment
                 else:
-                    print timedelta.days
+                    print "Nothing from assignee, using last comment %s" % bug.comments[-1].creation_time.replace(tzinfo=None)
+                    last_comment = bug.comments[-1].creation_time.replace(tzinfo=None)
+                if last_comment != None:
+                    timedelta = datetime.now() - last_comment
+                    if timedelta.days <= int(options.days_since_comment):
+                        if options.verbose:
+                            print "Skipping bug %s since it's had a comment within the past %s days" % (bug.id, options.days_since_comment)
+                        send_mail = False
+                        counter = counter - 1
+                        manual_notify.remove(bug)
+                    else:
+                        if options.verbose:
+                            print "This bug needs notification, it's been %s since last comment of note" % timedelta.days
+    
+            if send_mail:
+                if 'nobody' in assignee:
                     if options.verbose:
-                        print "This bug needs notification, it's been %s since last comment of note" % timedelta.days
-
-        if send_mail:
-            if 'nobody' in assignee:
-                if options.verbose:
-                    print "No one assigned to: %s, adding to manual notification list..." % bug.id
-                assignee = None
-            elif 'general@js.bugs' in assignee:
-                if options.verbose:
-                    print "No one assigned to JS bug: %s, adding to dmandelin's list..." % bug.id
-                add_to_managers('dmandelin@mozilla.com')
-            else:
-                if bug.assigned_to.real_name != None:
-                    if person != None:
-                        # check if assignee is already a manager
-                        if managers.has_key(person['bugzillaEmail']):
-                            add_to_managers(person['bugzillaEmail'])
-                        # otherwise we dig up the assignee's manager
-                        else:
-                            manager_email = person['manager']['dn'].split('mail=')[1].split(',')[0]
-                            if managers.has_key(manager_email):
-                                add_to_managers(manager_email)
-                            elif people.vices.has_key(manager_email):
-                                # we're already at the highest level we'll go
-                                if managers.has_key(assignee):
-                                    add_to_managers(assignee)
-                                else:
-                                    if options.verbose:
-                                        print "%s has a V-level for a manager, and is not in the manager list" % assignee
-                                    managers[person['mozillaMail']] = {}
-                                    add_to_managers(person['mozillaMail'])
+                        print "No one assigned to: %s, adding to manual notification list..." % bug.id
+                    assignee = None
+                elif 'general@js.bugs' in assignee:
+                    if options.verbose:
+                        print "No one assigned to JS bug: %s, adding to dmandelin's list..." % bug.id
+                    add_to_managers('dmandelin@mozilla.com', channel)
+                else:
+                    if bug.assigned_to.real_name != None:
+                        if person != None:
+                            # check if assignee is already a manager
+                            if managers.has_key(person['bugzillaEmail']):
+                                add_to_managers(person['bugzillaEmail'], channel)
+                            # otherwise we dig up the assignee's manager
                             else:
-                                # try to go up one level and see if we find a manager
-                                if people.people.has_key(manager_email):
-                                    person = dict(people.people[manager_email])
+                                # check for manager key first, a few people don't have them
+                                if person.has_key('manager') and person['manager'] != None:
                                     manager_email = person['manager']['dn'].split('mail=')[1].split(',')[0]
                                     if managers.has_key(manager_email):
-                                        add_to_managers(manager_email)
+                                        add_to_managers(manager_email, channel)
+                                    elif people.vices.has_key(manager_email):
+                                        # we're already at the highest level we'll go
+                                        if managers.has_key(assignee):
+                                            add_to_managers(assignee, channel)
+                                        else:
+                                            if options.verbose:
+                                                print "%s has a V-level for a manager, and is not in the manager list" % assignee
+                                            managers[person['mozillaMail']] = {}
+                                            add_to_managers(person['mozillaMail'], channel)
+                                    else:
+                                        # try to go up one level and see if we find a manager
+                                        if people.people.has_key(manager_email):
+                                            person = dict(people.people[manager_email])
+                                            manager_email = person['manager']['dn'].split('mail=')[1].split(',')[0]
+                                            if managers.has_key(manager_email):
+                                                add_to_managers(manager_email, channel)
+                                        else:
+                                            print "Manager could not be found: %s" % manager_email
                                 else:
-                                    print "Manager could not be found: %s" % manager_email
+                                    print "%s's entry doesn't list a manager! Let's ask them to update phonebook." % person['name']
 
     # Get yr nag on!
     for email, info in managers.items():
+        if email == 'dmandelin@mozilla.com':
+            print info['nagging'].items()
         if info.has_key('nagging'):
             print "\nRelMan Nag is ready to send the following email:\n<------ MESSAGE BELOW -------->"
-            toaddrs,msg = createEmail(manager_email=email, bugs=info['nagging'], version=options.version)
+            toaddrs,msg = createEmail(manager_email=email, channels=info['nagging'])
             print msg
             print "<------- END MESSAGE -------->\nWould you like to send now?"
             inp = raw_input('\n Please select y/Y to send or n/N to skip and continue to next email: ')
@@ -249,7 +269,7 @@ if __name__ == '__main__':
                 sendMail(toaddrs,msg,options.dryrun)
                 counter = counter - len(info['nagging'])
                 # take sent bugs out of manual notification list
-                for bug in info['nagging']:
+                for bug in info['nagging']['channel']['bugs']:
                     manual_notify.remove(bug)
 
     # Here's the manual notification list
