bce696728f278df14c04156d517924ee9c52341c
Lukas Blakk <lukasblakk@gmail.com>
2013-07-01 13:11:39 -0700

    pep8 cleanup on b2g_query_creator

--- scripts/b2g_query_creator.py
+++ scripts/b2g_query_creator.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import urllib2
-import re
 import datetime
 import subprocess
 import os
@@ -10,8 +8,9 @@ from argparse import ArgumentParser
 
 CONFIG_JSON = os.getcwd() + "/scripts/configs/config.json"
 config = json.load(open(CONFIG_JSON, 'r'))
-scripts_dir =  os.getcwd() + "/scripts/"
-queries_dir =  os.getcwd() + "/queries/"
+scripts_dir = os.getcwd() + "/scripts/"
+queries_dir = os.getcwd() + "/queries/"
+
 
 def createQuery(title, short_title, url, show_summary):
     file_name = queries_dir + str(datetime.date.today()) + '_' + short_title
@@ -21,21 +20,22 @@ def createQuery(title, short_title, url, show_summary):
     qf.write("query_url = \'" + url + "\'\n")
     qf.write("show_summary = \'" + str(show_summary) + "\'\n")
 
-    return file_name 
+    return file_name
+
 
 def createQueriesList(print_all):
     queries = []
     weekday = datetime.datetime.today().weekday()
     for url in urls:
         if weekday >= 0 and weekday < 5 and url[0] == 5:
-            queries.append(createQuery(title=url[1][0],short_title=url[1][1],url=url[1][2],show_summary=url[1][3]))
+            queries.append(createQuery(title=url[1][0], short_title=url[1][1], url=url[1][2], show_summary=url[1][3]))
         if weekday == 0 and url[0] == 0:
-            queries.append(createQuery(title=url[1][0],short_title=url[1][1],url=url[1][2],show_summary=url[1][3]))
+            queries.append(createQuery(title=url[1][0], short_title=url[1][1], url=url[1][2], show_summary=url[1][3]))
         if weekday == 3 and url[0] == 3:
-            queries.append(createQuery(title=url[1][0],short_title=url[1][1],url=url[1][2],show_summary=url[1][3]))
+            queries.append(createQuery(title=url[1][0], short_title=url[1][1], url=url[1][2], show_summary=url[1][3]))
     return queries
 
-# From RyanVM   
+# From RyanVM
 # b2g tef+/shira+ bugs needing uplift to v1_0_1
 # https://bugzilla.mozilla.org/buglist.cgi?order=Bug%20Number;bug_status=RESOLVED;bug_status=VERIFIED;resolution=FIXED;field0-0-0=cf_blocking_b2g;type0-0-0=anywordssubstr;value0-0-0=tef%2B%20shira%2B;field0-1-0=component;type0-1-0=nowords;value0-1-0=Gaia%20Server%20Security%20Payments%20Hardware%20Builds;field0-2-0=cf_last_resolved;type0-2-0=greaterthaneq;value0-2-0=2013-01-25;field0-3-0=cf_status_b2g18_1_0_1;type0-3-0=nowordssubstr;value0-3-0=fixed%20verified%20unaffected%20wontfix%20disabled;field0-4-0=status_whiteboard;type0-4-0=notsubstring;value0-4-0=NO_UPLIFT;
 
@@ -71,41 +71,41 @@ qe1_blockers_url = "https://bugzilla.mozilla.org/buglist.cgi?f1=target_milestone
 # TODO - sort the queries according to a priority flag
 # TODO - batch up by query name (so sec & non-sec get in the same output)
 urls = [
-    (5,["Unlanded TEF (Sec) Bugs", "unlanded_tef_sec_url", unlanded_tef_sec_url, 0]),
-    (5,["Unlanded TEF Bugs", "unlanded_tef", unlanded_tef_url, 1]),
-    (5,["Unlanded LEO (Sec) Bugs", "unlanded_leo_sec_url", unlanded_leo_sec_url, 0]),
-    (5,["Unlanded LEO Bugs", "unlanded_leo", unlanded_leo_url, 1]),
-    (5,["Blocker Bugs with Need-Info? (Sec)", "needinfo_sec_blockers", needinfo_sec_blockers_url, 0]),
-    (5,["Blocker Bugs with Need-Info?", "needinfo_blockers", needinfo_blockers_url, 1]),
-    (0,["Bugs Blocking TEF (Sec)", "unfixed_tef_sec", unfixed_tef_sec_url, 0]),
-    (0,["Bugs Blocking TEF", "unfixed_tef", unfixed_tef_url, 1]),
-    (0,["Bugs Blocking LEO (Sec)", "unfixed_leo_sec", unfixed_leo_sec_url, 0]),
-    (0,["Bugs Blocking LEO", "unfixed_leo", unfixed_leo_url, 1]),
-    (0,["QE1 Blocking Bugs (Sec)", "blocking_qe1_sec", qe1_sec_blockers_url, 0]),
-    (0,["QE1 Blocking Bugs", "blocking_qe1", qe1_blockers_url, 1]),
-    (3,["TEF Blocker Bugs, untouched this week", "untouched_tef_blockers", untouched_tef_url, 1]),
-    (3,["LEO Blocker Bugs, untouched this week", "untouched_leo_blockers", untouched_leo_url, 1]),
-    (3,["TEF Blocker Bugs (Sec), untouched this week", "untouched_tef_sec_blockers", untouched_tef_sec_url, 0]),
-    (3,["LEO Blocker Bugs (Sec), untouched this week", "untouched_leo_sec_blockers", untouched_leo_sec_url, 0]),
-    ]
+    (5, ["Unlanded TEF (Sec) Bugs", "unlanded_tef_sec_url", unlanded_tef_sec_url, 0]),
+    (5, ["Unlanded TEF Bugs", "unlanded_tef", unlanded_tef_url, 1]),
+    (5, ["Unlanded LEO (Sec) Bugs", "unlanded_leo_sec_url", unlanded_leo_sec_url, 0]),
+    (5, ["Unlanded LEO Bugs", "unlanded_leo", unlanded_leo_url, 1]),
+    (5, ["Blocker Bugs with Need-Info? (Sec)", "needinfo_sec_blockers", needinfo_sec_blockers_url, 0]),
+    (5, ["Blocker Bugs with Need-Info?", "needinfo_blockers", needinfo_blockers_url, 1]),
+    (0, ["Bugs Blocking TEF (Sec)", "unfixed_tef_sec", unfixed_tef_sec_url, 0]),
+    (0, ["Bugs Blocking TEF", "unfixed_tef", unfixed_tef_url, 1]),
+    (0, ["Bugs Blocking LEO (Sec)", "unfixed_leo_sec", unfixed_leo_sec_url, 0]),
+    (0, ["Bugs Blocking LEO", "unfixed_leo", unfixed_leo_url, 1]),
+    (0, ["QE1 Blocking Bugs (Sec)", "blocking_qe1_sec", qe1_sec_blockers_url, 0]),
+    (0, ["QE1 Blocking Bugs", "blocking_qe1", qe1_blockers_url, 1]),
+    (3, ["TEF Blocker Bugs, untouched this week", "untouched_tef_blockers", untouched_tef_url, 1]),
+    (3, ["LEO Blocker Bugs, untouched this week", "untouched_leo_blockers", untouched_leo_url, 1]),
+    (3, ["TEF Blocker Bugs (Sec), untouched this week", "untouched_tef_sec_blockers", untouched_tef_sec_url, 0]),
+    (3, ["LEO Blocker Bugs (Sec), untouched this week", "untouched_leo_sec_blockers", untouched_leo_sec_url, 0]),
+]
+
 
 def cleanUp():
     for file in os.listdir(queries_dir):
         if file.startswith(str(datetime.date.today())):
-            os.remove(os.path.join(queries_dir,file))
+            os.remove(os.path.join(queries_dir, file))
 
 if __name__ == '__main__':
     parser = ArgumentParser(__doc__)
     parser.set_defaults(
         queries_only=False,
-        )
+    )
     parser.add_argument("-q", "--queries-only", dest="queries_only", action="store_true",
-            help="just create and print queries")
+                        help="just create and print queries")
 
     options, args = parser.parse_known_args()
-    
     queries = createQueriesList(print_all=options.queries_only)
-    
+
     if options.queries_only:
         for url in urls:
             print url
@@ -115,7 +115,7 @@ if __name__ == '__main__':
             "-t", "daily_b2g_email",
             "--no-verification",
             "-m", config['ldap_username'],
-            "-p", config['ldap_password'],]
+            "-p", config['ldap_password']]
         for query in queries:
             command.append('-q')
             command.append(query)
@@ -125,4 +125,3 @@ if __name__ == '__main__':
         command.extend(args)
         subprocess.call(command)
         cleanUp()
-
