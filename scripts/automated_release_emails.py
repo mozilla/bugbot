88ea3b56a2b662441c8aeeb9a5e077916f7f0b7b
Anoop Valluthadam <anoopvalluthadam@gmail.com>
2015-10-19 11:38:11 +0530

    Port to Python Requests

--- scripts/automated_release_emails.py
+++ scripts/automated_release_emails.py
@@ -1,7 +1,7 @@
 # Post-Mortem and Sign-off automated reminder
 # Daily cronjob, 6am PT
 
-import urllib2
+import requests
 import datetime
 import re
 import os
@@ -38,7 +38,7 @@ def sendMail(toaddr, options):
 
 def getTemplateValue(url):
     version_regex = re.compile(".*<p>(.*)</p>.*")
-    template_page = urllib2.urlopen(url).read().replace('\n', '')
+    template_page = str(requests.get(url).text.encode('utf-8')).replace('\n', '')
     parsed_template = version_regex.match(template_page)
     return parsed_template.groups()[0]
 
