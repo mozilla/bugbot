5b16ffb07e7a4a021334b49725b126f95ed6e404
Anoop Valluthadam <anoopvalluthadam@gmail.com>
2015-11-05 17:32:32 +0530

    Tests for Phonebook: comparing the default dict with phonebook data

--- auto_nag/tests/test_phonebook.py
+++ auto_nag/tests/test_phonebook.py
@@ -1,14 +1,26 @@
 
-import datetime
-
-from auto_nag.bugzilla.utils import get_project_root_path
-from auto_nag.bugzilla.agents import BMOAgent
 from auto_nag.scripts.phonebook import PhonebookDirectory
 
 
 class TestPhonebook:
+    def __init__(self):
+        self.demo_dict = {u'email@example.com': {
+                          u'bugzillaEmail': u'email@example.com',
+                          u'name': u'name',
+                          u'title': u'vice/director test',
+                          u'phones': u'string of numbers & assignments',
+                          u'ext': u'XXX',
+                          u'manager': {
+                                    u'dn': u'mail=manager@mozilla.com,o=com,dc=mozilla',
+                                    u'cn': u'Manager Name'
+                          },
+                          u'ims': [],
+                          u'mozillaMail': u'email'}}
+
     def test_init(self):
         pd = PhonebookDirectory(dryrun=True)
-        assert pd.people_by_bzmail
-        assert pd.managers
-        assert pd.vices
+        assert not len(set(self.demo_dict) ^ set(pd.people_by_bzmail))
+        assert not len(set(pd.managers['email']) ^
+                       set(self.demo_dict['email@example.com']))
+        assert not len(set(pd.vices['email']) ^
+                       set(self.demo_dict['email@example.com']))
