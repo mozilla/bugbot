67f0ae65ac37b6ba80182fabcefce380a7ef5c2c
Julien Cristau <jcristau@mozilla.com>
2016-10-24 12:02:32 +0200

    test: check that summary is only included in email for public bugs
    Mock a minimal "private" bug and check that the generated email excludes
    its summary.

--- auto_nag/tests/test_email_nag.py
+++ auto_nag/tests/test_email_nag.py
@@ -1,8 +1,9 @@
 
 import datetime
 
-from auto_nag.bugzilla.utils import get_project_root_path
+from auto_nag.bugzilla.utils import get_project_root_path, getVersions
 from auto_nag.bugzilla.agents import BMOAgent
+from auto_nag.bugzilla.models import Bug
 from auto_nag.scripts.phonebook import PhonebookDirectory
 from auto_nag.scripts.email_nag import (get_last_manager_comment,
                                         get_last_assignee_comment,
@@ -15,6 +16,14 @@ class TestEmailNag:
         bmo = BMOAgent()
         self.bug = bmo.get_bug(bug_number)
         self.people = PhonebookDirectory(dryrun=True)
+        hidden_dict = {
+                'id': 123456,
+                'summary': 'hide me',
+                'assigned_to': {'real_name': 'nobody'},
+                'groups': [{'name': 'security group'}]}
+        hidden_dict.update({'cf_status_firefox' + version: '---'
+                            for version in getVersions()})
+        self.hidden_bug = Bug.from_dict(hidden_dict)
         assignee = 'email@example.com'
         self.manager = {'mozillaMail': 'test@mozilla.com',
                         'bugzillaEmail': 'demo@bugzilla.com'}
@@ -39,6 +48,15 @@ class TestEmailNag:
                                                self.people, False,
                                                'Test@test.com')
         contents = ('From', 'To', 'CC', 'Subject:', '= Next Actions =',
+                    '= Queries =', 'Sincerely,', self.bug.summary)
+        assert '@' in toaddrs[0]
+        assert all(content in message for content in contents)
+        query = {'Test': {'bugs': [self.hidden_bug]}}
+        toaddrs, message = generateEmailOutput('Test', query, 'daily_email',
+                                               self.people, False,
+                                               'Test@test.com')
+        contents = ('From', 'To', 'CC', 'Subject:', '= Next Actions =',
                     '= Queries =', 'Sincerely,')
         assert '@' in toaddrs[0]
         assert all(content in message for content in contents)
+        assert self.hidden_bug.summary not in message
