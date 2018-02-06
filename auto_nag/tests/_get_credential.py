034173ae67f25a2cc7473062afe7cfb07dca1f5d
Julien Cristau <jcristau@mozilla.com>
2018-02-06 10:24:14 +0100

    Make sys.path hack in _get_credentials.py script slightly less horrible
    Rather than adding the path to the auto_nag/bugzilla package to
    sys.path, use the actual project root path, and then import stuff from
    auto_nag.bugzilla.

--- auto_nag/tests/_get_credential.py
+++ auto_nag/tests/_get_credential.py
@@ -3,11 +3,9 @@
 import os
 import inspect
 import sys
-currentdir = os.path.dirname(os.path.abspath(
-                             inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-util_dir = parentdir + '/bugzilla'
-sys.path.insert(0, util_dir)
-import utils
+currentdir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0, project_root)
+from auto_nag.bugzilla import utils
 
 utils.get_credentials()
