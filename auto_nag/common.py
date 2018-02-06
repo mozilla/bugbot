631aae59e54b1fc5f96072969c3c6c89bd63c33a
Sylvestre Ledru <sylvestre@debian.org>
2018-02-06 11:29:13 +0100

    revert my stupid change

--- auto_nag/common.py
+++ auto_nag/common.py
@@ -6,7 +6,7 @@ def loadJSON(url):
     return json.load(urlopen(url))
 
 
-def get_current_versions(jsonVersion, key):
+def getVersion(jsonVersion, key):
     # In X.Y, we just need X
     version = jsonVersion[key].split(".")
     return version[0]
@@ -14,7 +14,7 @@ def get_current_versions(jsonVersion, key):
 
 def getCurrentVersion():
     jsonContent = loadJSON("https://product-details.mozilla.org/1.0/firefox_versions.json")
-    esr_next_version = get_current_versions(jsonContent, "FIREFOX_ESR_NEXT")
+    esr_next_version = getVersion(jsonContent, "FIREFOX_ESR_NEXT")
     if esr_next_version:
         esr_version = esr_next_version
     else:
@@ -22,10 +22,11 @@ def getCurrentVersion():
         # For example, with 52.6, esr_next doesn't exist
         # But it will exist, once 60 is released
         # esr_next will be 60
-        esr_version = get_current_versions(jsonContent, "FIREFOX_ESR")
+        esr_version = getVersion(jsonContent, "FIREFOX_ESR")
 
-    return {"central": get_current_versions(jsonContent, "FIREFOX_NIGHTLY"),
-            "beta": get_current_versions(jsonContent, "LATEST_FIREFOX_DEVEL_VERSION"),
+    return {"central": getVersion(jsonContent, "FIREFOX_NIGHTLY"),
+            "aurora": getVersion(jsonContent, "FIREFOX_AURORA"),
+            "beta": getVersion(jsonContent, "LATEST_FIREFOX_DEVEL_VERSION"),
             "esr": esr_version,
-            "release": get_current_versions(jsonContent, "LATEST_FIREFOX_VERSION"),
+            "release": getVersion(jsonContent, "LATEST_FIREFOX_VERSION"),
             }
