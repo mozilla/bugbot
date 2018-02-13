e033c6975a380dd3d9b49ffc4c05ad50912f7cb9
Julien Cristau <jcristau@mozilla.com>
2018-02-13 15:54:33 +0100

    Set User-Agent to "relman-auto-nag" for bugzilla queries
    Fixes #82

--- auto_nag/bugzilla/agents.py
+++ auto_nag/bugzilla/agents.py
@@ -1,6 +1,16 @@
 import os
+import httplib2
 from auto_nag.bugzilla.models import BugSearch, Bug
 from auto_nag.bugzilla.utils import urljoin, qs, hide_personal_info
+
+
+class Http(httplib2.Http):
+    def request(self, uri, method='GET', body=None, headers=None, redirections=5, connection_type=None):
+        if headers is None:
+            headers = {}
+        if 'user-agent' not in headers:
+            headers['user-agent'] = 'relman-auto-nag'
+        return super(Http, self).request(uri, method=method, body=body, headers=headers, redirections=redirections, connection_type=connection_type)
 
 
 class InvalidAPI_ROOT(Exception):
@@ -18,8 +28,8 @@ class BugzillaAgent(object):
             if not api_root:
                 raise InvalidAPI_ROOT
         self.API_ROOT = api_root
-
         self.api_key = api_key
+        self.http = Http()
 
     def get_bug(self, bug, include_fields='_default,token,cc,keywords,whiteboard,comments', exclude_fields=None, params={}):
         params['include_fields'] = [include_fields]
@@ -27,14 +37,14 @@ class BugzillaAgent(object):
 
         url = urljoin(self.API_ROOT, 'bug/%s?%s' % (bug, self.qs(**params)))
         try:
-            return Bug.get(url)
+            return Bug.get(url, http=self.http)
         except Exception as e:
             raise Exception(hide_personal_info(str(e)))
 
     def get_bug_list(self, params={}):
         url = urljoin(self.API_ROOT, 'bug/?%s' % (self.qs(**params)))
         try:
-            return BugSearch.get(url).bugs
+            return BugSearch.get(url, http=self.http).bugs
         except Exception as e:
             raise Exception(hide_personal_info(str(e)))
 
