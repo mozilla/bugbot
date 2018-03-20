22dcb76200073ec5645b296d3235c5dd90ccf36d
Sylvestre Ledru <sylvestre@debian.org>
2018-03-20 10:12:08 +0100

    add PYTHONPATH=.

--- runauto_nag.sh
+++ runauto_nag.sh
@@ -2,6 +2,6 @@
 PATH_SCRIPT="$( cd "$(dirname "$0")" ; pwd -P )"
 . $PATH_SCRIPT/venv/bin/activate
 cd $PATH_SCRIPT
-python -m auto_nag.scripts.query_creator
-python -m auto_nag.scripts.rm_query_creator
-python -m auto_nag.scripts.no_assignee
+PYTHONPATH=. python -m auto_nag.scripts.query_creator
+PYTHONPATH=. python -m auto_nag.scripts.rm_query_creator
+PYTHONPATH=. python -m auto_nag.scripts.no_assignee
