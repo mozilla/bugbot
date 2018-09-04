f85988502bb7f8c91d7cb44fd90bc904d70c0e62
Calixte Denizet <calixte.denizet@gmail.com>
2018-09-04 11:49:02 +0200

    Fix the no_assignee test

--- auto_nag/tests/test_email_no_assignee.py
+++ auto_nag/tests/test_email_no_assignee.py
@@ -2,12 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import unittest
 
 from auto_nag.scripts.no_assignee import NoAssignee
 
 
-class TestEmailNoAssignee:
+class TestEmailNoAssignee(unittest.TestCase):
 
     def test_nobody(self):
         bugids = NoAssignee().get_bugs('2011-01-01', bug_ids=[229367, 400095])
-        assert bugids == [229367]
+        assert bugids == {'229367': None}
