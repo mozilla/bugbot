70cac26689a279e30b0c6f93e48a0eae73d82f79
Sylvestre Ledru <sylvestre@mozilla.com>
2018-10-28 15:57:55 +0100

    auto fix old P1 => P3

--- runauto_nag.sh
+++ runauto_nag.sh
@@ -62,3 +62,7 @@ python -m auto_nag.scripts.mismatch-priority-tracking
 # Bugs where the reporter has a needinfo
 # Pretty common
 python -m auto_nag.scripts.reporter_with_ni
+
+# P1 bug with no activity for more than 24 weeks
+# Pretty common
+python -m auto_nag.scripts.old_p1_bug
