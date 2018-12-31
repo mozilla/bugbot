058276f0c9c5bd7227fd0c357b5ee417a5bef3b0
Sylvestre Ledru <sylvestre@debian.org>
2018-12-31 17:25:57 +0100

    support of esr for P3, P4, P5 tracked bugs

---
+++ auto_nag/scripts/mismatch-priority-tracking-esr.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from auto_nag.bzcleaner import BzCleaner
+from auto_nag.bugzilla.utils import getVersions
+
+
+class MismatchPrioTrackESR(BzCleaner):
+    def __init__(self):
+        super(MismatchPrioTrackESR, self).__init__()
+
+    def description(self):
+        return 'Bug tracked for esr with a bad priority (P3, P4 or P5)'
+
+    def name(self):
+        return 'mismatch-priority-tracking'
+
+    def template(self):
+        return 'mismatch-priority-tracking.html'
+
+    def subject(self):
+        return self.description()
+
+    def ignore_date(self):
+        return True
+
+    def get_bz_params(self, date):
+        _, _, _, esr_version = getVersions()
+        print(esr_version)
+        value = ','.join(['', 'affected'])
+        params = {
+            'resolution': [
+                '---',
+                'FIXED',
+                'INVALID',
+                'WONTFIX',
+                'DUPLICATE',
+                'WORKSFORME',
+                'INCOMPLETE',
+                'SUPPORT',
+                'EXPIRED',
+                'MOVED',
+            ],
+            'priority': ['P3', 'P4', 'P5'],
+            'f1': 'OP',
+            'f2': 'cf_tracking_firefox' + esr_version,
+            'o2': 'anyexact',
+            'v2': ','.join(['+', 'blocking']),
+            'f3': 'cf_status_firefox' + esr_version,
+            'o3': 'anyexact',
+            'v3': value,
+            'o4': 'anyexact',
+            'f5': 'CP',
+            'o6': 'notsubstring',
+        }
+        return params
+
+    def get_autofix_change(self):
+        return {
+            'comment': {
+                'body': 'Changing the priority to p1 as the bug is tracked by a release manager for the current esr.\nSee https://github.com/mozilla/bug-handling/blob/master/policy/triage-bugzilla.md#how-do-you-triage for more information'
+            },
+            'priority': 'p2',
+        }
+
+
+
+if __name__ == '__main__':
+    MismatchPrioTrackESR().run()
