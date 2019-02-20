bd77e8e813ea86700db539ae1d959b0442a9316a
Marco <mcastelluccio@mozilla.com>
2019-02-20 16:16:50 +0100

    Add a script to automatically assign components to untriaged bugs  (#374)
    * Add a script to automatically assign components to untriaged bugs
    
    * Ignore downloaded bugbug models
    
    * Fix nits
    
    * More stuff
    
    * Remove unused imports
    
    * Remove pword

---
+++ auto_nag/scripts/component.py
@@ -0,0 +1,88 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from auto_nag.bugbug_utils import BugbugScript
+from bugbug.models.component import ComponentModel
+from auto_nag import utils
+
+
+class Component(BugbugScript):
+    def __init__(self):
+        super(Component, self).__init__()
+        self.model = ComponentModel.load(self.retrieve_model('component'))
+
+    def description(self):
+        return 'Assign a component to untriaged bugs'
+
+    def name(self):
+        return 'component'
+
+    def template(self):
+        return 'component.html'
+
+    def subject(self):
+        return '[Using ML] Assign a component to untriaged bugs'
+
+    def columns(self):
+        return ['id', 'summary', 'component', 'confidence']
+
+    def get_bz_params(self, date):
+        start_date, end_date = self.get_dates(date)
+        params = {
+            'component': 'Untriaged',
+            'chfield': '[Bug creation]',
+            'chfieldfrom': start_date,
+            'chfieldto': end_date,
+        }
+
+        return params
+
+    def get_data(self):
+        return {'bugs': {}}
+
+    def bughandler(self, bug, data):
+        data['bugs'][bug['id']] = bug
+
+    def get_bugs(self, date='today', bug_ids=[]):
+        # Retrieve bugs to analyze.
+        all_bug_data = super(Component, self).get_bugs(date=date, bug_ids=bug_ids)
+
+        bugs = all_bug_data['bugs']
+
+        # Retrieve history.
+        self.retrieve_history(bugs)
+
+        # Retrieve comments.
+        self.retrieve_comments(bugs)
+
+        # Retrieve attachments.
+        self.retrieve_attachments(bugs)
+
+        bugs = list(bugs.values())
+
+        # Analyze bugs.
+        probs = self.model.classify(bugs, True)
+
+        # Get the encoded component.
+        indexes = probs.argmax(axis=-1)
+        # Apply inverse transformation to get the component name from the encoded value.
+        components = self.model.clf._le.inverse_transform(indexes)
+
+        result = {}
+        for bug, prob, index, component in zip(bugs, probs, indexes, components):
+            # Only return result for which we are sure enough.
+            if prob[index] >= utils.get_config(self.name(), 'confidence_threshold'):
+                bug_id = str(bug['id'])
+                result[bug_id] = {
+                    'id': bug_id,
+                    'summary': self.get_summary(bug),
+                    'component': component,
+                    'confidence': prob[index],
+                }
+
+        return result
+
+
+if __name__ == '__main__':
+    Component().run()
--- auto_nag/scripts/regression.py
+++ auto_nag/scripts/regression.py
@@ -2,39 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from libmozdata.bugzilla import Bugzilla
-from auto_nag.bzcleaner import BzCleaner
-import lzma
-import shutil
+from auto_nag.bugbug_utils import BugbugScript
 from bugbug.models.regression import RegressionModel
-from urllib.request import urlretrieve
-import requests
 
 
-class Regression(BzCleaner):
+class Regression(BugbugScript):
     def __init__(self):
         super(Regression, self).__init__()
-        MODEL_URL = 'https://index.taskcluster.net/v1/task/project.releng.services.project.testing.bugbug_train.latest/artifacts/public/regressionmodel.xz'
-        r = requests.head(MODEL_URL, allow_redirects=True)
-        new_etag = r.headers['ETag']
-
-        try:
-            with open('regressionmodel.etag', 'r') as f:
-                old_etag = f.read()
-        except IOError:
-            old_etag = None
-
-        if old_etag != new_etag:
-            urlretrieve(MODEL_URL, 'regressionmodel.xz')
-
-            with lzma.open('regressionmodel.xz', 'rb') as input_f:
-                with open('regressionmodel', 'wb') as output_f:
-                    shutil.copyfileobj(input_f, output_f)
-
-            with open('regressionmodel.etag', 'w') as f:
-                f.write(new_etag)
-
-        self.model = RegressionModel.load('regressionmodel')
+        self.model = RegressionModel.load(self.retrieve_model('regression'))
 
     def description(self):
         return 'Get bugs with missing regression keyword'
@@ -47,12 +22,6 @@ class Regression(BzCleaner):
 
     def subject(self):
         return '[Using ML] Bugs with missing regression keyword'
-
-    def ignore_bug_summary(self):
-        return False
-
-    def all_include_fields(self):
-        return True
 
     def columns(self):
         return ['id', 'summary']
@@ -100,28 +69,6 @@ class Regression(BzCleaner):
                 data['others'][bug['id']] = bug
         data['summaries'][bug['id']] = self.get_summary(bug)
 
-    def retrieve_comments(self, bugs):
-        """Retrieve bug comments"""
-
-        def comment_handler(bug, bug_id):
-            bugs[int(bug_id)]['comments'] = bug['comments']
-
-        Bugzilla(
-            bugids=[bug_id for bug_id in bugs.keys()],
-            commenthandler=comment_handler,
-            comment_include_fields=['text'],
-        ).get_data().wait()
-
-    def retrieve_history(self, bugs):
-        """Retrieve bug history"""
-
-        def history_handler(bug):
-            bugs[int(bug['id'])]['history'] = bug['history']
-
-        Bugzilla(
-            bugids=[bug_id for bug_id in bugs.keys()], historyhandler=history_handler
-        ).get_data().wait()
-
     def remove_using_history(self, bugs):
         to_remove = set()
         for bug_id, bug in bugs.items():
@@ -137,25 +84,6 @@ class Regression(BzCleaner):
 
         for bug_id in to_remove:
             del bugs[bug_id]
-
-    def retrieve_attachments(self, bugs):
-        """Retrieve bug attachments"""
-
-        def attachment_handler(bug, bug_id):
-            bugs[int(bug_id)]['attachments'] = bug
-
-        Bugzilla(
-            bugids=[bug_id for bug_id in bugs.keys()],
-            attachmenthandler=attachment_handler,
-            attachment_include_fields=[
-                'id',
-                'is_obsolete',
-                'flags',
-                'is_patch',
-                'creator',
-                'content_type',
-            ],
-        ).get_data().wait()
 
     def get_bugs(self, date='today', bug_ids=[]):
         # Retrieve bugs to analyze.
@@ -181,7 +109,6 @@ class Regression(BzCleaner):
         probs = self.model.classify(bugs, True)
 
         reg_bugids = {bug['id'] for bug, prob in zip(bugs, probs) if prob[1] > 0.9}
-        print(len(reg_bugids))
 
         # Add bugs that are certainly regressions.
         reg_bugids |= all_bug_data['regressions']
