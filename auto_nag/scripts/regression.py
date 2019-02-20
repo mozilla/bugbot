90b5ba797349eb8709b65f8e7f29b3b6dddf9501
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-02-20 15:29:18 +0100

    Remove unused imports

--- auto_nag/scripts/regression.py
+++ auto_nag/scripts/regression.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from libmozdata.bugzilla import Bugzilla
-from auto_nag.bzcleaner import BzCleaner
 from auto_nag.bugbug_utils import BugbugScript
 from bugbug.models.regression import RegressionModel
 
