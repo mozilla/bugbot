43d7b653f7949813d0f1832f63f3cda21e48a68d
Marco <mcastelluccio@mozilla.com>
2019-02-26 08:45:48 +0100

    Check that the version of bugbug used is the same as the one we used for training (#385)

---
+++ check_bugbug_version.py
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import requests
+
+
+def get_bugbug_version(requirements):
+    for line in requirements.splitlines():
+        if line.startswith('https://github.com/marco-c/bugbug/archive/'):
+            return line[len('https://github.com/marco-c/bugbug/archive/'):line.index('.tar')]
+
+    return None
+
+
+with open('requirements.txt') as f:
+    autonag_bugbug_version = get_bugbug_version(f.read())
+
+r = requests.get('https://raw.githubusercontent.com/mozilla/release-services/master/src/bugbug/train/requirements.txt')
+train_bugbug_version = get_bugbug_version(r.text)
+
+assert autonag_bugbug_version == train_bugbug_version, '{} should be {}'.format(autonag_bugbug_version, train_bugbug_version)
