c5d9629f8c2710184fdecaa3f07f0f0993ed2254
Sylvestre Ledru <sledru@mozilla.com>
2019-02-27 14:48:06 +0100

    Show descriptions & Update of the doc to run the cron jobs (#411)
    * update of the doc to run the cron jobs
    
    * Show the bug summary
    
    * also fix the python code

--- auto_nag/scripts/version_affected.py
+++ auto_nag/scripts/version_affected.py
@@ -23,6 +23,9 @@ class VersionAffected(BzCleaner):
     def subject(self):
         return self.description()
 
+    def ignore_bug_summary(self):
+        return False
+
     def get_bz_params(self, date):
         params = {
             'resolution': ['---', 'FIXED'],
