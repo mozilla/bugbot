412d8db63ec2ec72acd4ba1305f70e37e9de7b29
calixteman <cdenizet@mozilla.com>
2019-03-22 17:15:13 +0100

    tracked_bad_severity: autofix severity to 'major' (#470)

--- auto_nag/scripts/tracked_bad_severity.py
+++ auto_nag/scripts/tracked_bad_severity.py
@@ -3,12 +3,13 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from auto_nag.bzcleaner import BzCleaner
-from auto_nag.bugzilla.utils import getVersions
+from auto_nag import utils
 
 
 class TrackedBadSeverity(BzCleaner):
     def __init__(self):
         super(TrackedBadSeverity, self).__init__()
+        self.versions = utils.get_checked_versions()
 
     def description(self):
         return 'Bug tracked in a release but with a small severity'
@@ -25,35 +26,37 @@ class TrackedBadSeverity(BzCleaner):
     def ignore_date(self):
         return True
 
+    def has_enough_data(self):
+        return bool(self.versions)
+
     def get_bz_params(self, date):
         # TODO add support for ESR here?
-        release_version, beta_version, central_version, esr_version = getVersions()
         value = ','.join(['affected', 'fixed'])
         params = {
             'bug_severity': ['normal', 'minor', 'trivial', 'enhancement'],
             'f1': 'OP',
             'j1': 'OR',
             'f2': 'OP',
-            'f3': 'cf_tracking_firefox' + release_version,
+            'f3': utils.get_flag(self.versions['release'], 'tracking', 'release'),
             'o3': 'equals',
             'v3': 'blocking',
-            'f4': 'cf_status_firefox' + release_version,
+            'f4': utils.get_flag(self.versions['release'], 'status', 'release'),
             'o4': 'anyexact',
             'v4': value,
             'f5': 'CP',
             'f6': 'OP',
-            'f7': 'cf_tracking_firefox' + beta_version,
+            'f7': utils.get_flag(self.versions['beta'], 'tracking', 'beta'),
             'o7': 'equals',
             'v7': 'blocking',
-            'f8': 'cf_status_firefox' + beta_version,
+            'f8': utils.get_flag(self.versions['beta'], 'status', 'beta'),
             'o8': 'anyexact',
             'v8': value,
             'f9': 'CP',
             'f10': 'OP',
-            'f11': 'cf_tracking_firefox' + central_version,
+            'f11': utils.get_flag(self.versions['central'], 'tracking', 'central'),
             'o11': 'equals',
             'v11': 'blocking',
-            'f12': 'cf_status_firefox' + central_version,
+            'f12': utils.get_flag(self.versions['central'], 'status', 'central'),
             'o12': 'anyexact',
             'v12': value,
             'f13': 'CP',
@@ -61,6 +64,9 @@ class TrackedBadSeverity(BzCleaner):
         }
 
         return params
+
+    def get_autofix_change(self):
+        return {'severity': 'major'}
 
 
 if __name__ == '__main__':
