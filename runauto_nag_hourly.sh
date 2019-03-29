fa0544a7cc72cba80d43e21381cbcce9074f9fd6
calixteman <cdenizet@mozilla.com>
2019-03-29 13:14:47 +0100

    Specify python2 for python 2 tools (#495)

--- runauto_nag_hourly.sh
+++ runauto_nag_hourly.sh
@@ -17,75 +17,75 @@ fi
 export PYTHONPATH=.
 
 # Clean the log files
-python -m auto_nag.log --clean
+python2 -m auto_nag.log --clean
 
 # Bug fixed without assignee
 # very common
-python -m auto_nag.scripts.no_assignee
+python2 -m auto_nag.scripts.no_assignee
 
 # Bug closed with the leave open keyword
 # very common
-python -m auto_nag.scripts.leave_open
+python2 -m auto_nag.scripts.leave_open
 
 # has a STR without flag has_str
 # common
-# python -m auto_nag.scripts.has_str_no_hasstr
+# python2 -m auto_nag.scripts.has_str_no_hasstr
 
 # hasRegressionRange is set but no regression keyword
 # common
-python -m auto_nag.scripts.has_regression_range_no_keyword
+python2 -m auto_nag.scripts.has_regression_range_no_keyword
 
 # Closes crash bug without any crashes for the last 12 weeks
 # pretty common
-python -m auto_nag.scripts.no_crashes
+python2 -m auto_nag.scripts.no_crashes
 
 # Unconfirmed bugs with an assignee (with autofix)
 # Pretty common
-python -m auto_nag.scripts.assignee_but_unconfirmed
+python2 -m auto_nag.scripts.assignee_but_unconfirmed
 
 # List bug with the meta keyword but not [meta] in the title
 # Pretty common
-python -m auto_nag.scripts.meta_summary_missing
+python2 -m auto_nag.scripts.meta_summary_missing
 
 # List bug without the meta keyword with [meta] in the title (with autofix)
 # Pretty common
-python -m auto_nag.scripts.summary_meta_missing
+python2 -m auto_nag.scripts.summary_meta_missing
 
 # List reopened bugs with invalid nightly status flag
 # Pretty common
-python -m auto_nag.scripts.nightly_reopened
+python2 -m auto_nag.scripts.nightly_reopened
 
 # Bug closed with the stalled keyword
 # Pretty rare
-python -m auto_nag.scripts.stalled
+python2 -m auto_nag.scripts.stalled
 
 # Bugs with missing beta status
 # Pretty rare
-python -m auto_nag.scripts.missing_beta_status
+python2 -m auto_nag.scripts.missing_beta_status
 
 # Bugs with STR and no regression-range
 # Pretty rare
-python -m auto_nag.scripts.has_str_no_range
+python2 -m auto_nag.scripts.has_str_no_range
 
 # Notify bugs tracked (+ or blocking)
 # with P3, P4 or P5 priorities for the ongoing releases
 # Pretty common
-python -m auto_nag.scripts.mismatch-priority-tracking-esr
-python -m auto_nag.scripts.mismatch-priority-tracking-release
-python -m auto_nag.scripts.mismatch-priority-tracking-beta
-python -m auto_nag.scripts.mismatch-priority-tracking-nightly
+python2 -m auto_nag.scripts.mismatch-priority-tracking-esr
+python2 -m auto_nag.scripts.mismatch-priority-tracking-release
+python2 -m auto_nag.scripts.mismatch-priority-tracking-beta
+python2 -m auto_nag.scripts.mismatch-priority-tracking-nightly
 
 # Bug is tracked for a release but the bug severity is small
 # pretty common
-python -m auto_nag.scripts.tracked_bad_severity
+python2 -m auto_nag.scripts.tracked_bad_severity
 
 # Move info (signatures, product/component) from/to bugs & their dups
 # Pretty common
-python -m auto_nag.scripts.copy_duplicate_info
+python2 -m auto_nag.scripts.copy_duplicate_info
 
 # Suggest components for untriaged bugs (hourly, list only bugs on which we acted)
 python3 -m auto_nag.scripts.component --frequency hourly
 
 # Send a mail if the logs are not empty
 # MUST ALWAYS BE THE LAST COMMAND
-python -m auto_nag.log --send
+python2 -m auto_nag.log --send
