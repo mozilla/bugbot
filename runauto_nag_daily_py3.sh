64bc17522d7952ab0381969774113c74073ebf8f
Marco <mcastelluccio@mozilla.com>
2019-04-10 14:32:19 +0200

    Actually run the defect/enhancement/task script (#540)

--- runauto_nag_daily_py3.sh
+++ runauto_nag_daily_py3.sh
@@ -15,6 +15,9 @@ python -m auto_nag.scripts.regression
 # Suggest components for untriaged bugs (daily, full list without confidence threshold)
 python -m auto_nag.scripts.component --frequency daily
 
+# Try to detect potential wrong bug types using bugbug
+python -m auto_nag.scripts.defectenhancementtask
+
 # Send a mail if the logs are not empty
 # MUST ALWAYS BE THE LAST COMMAND
 python -m auto_nag.log --send
