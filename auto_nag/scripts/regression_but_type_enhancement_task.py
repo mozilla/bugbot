94d92e96e965c18398f899dbb72c3f3ea00da17b
Sylvestre Ledru <sledru@mozilla.com>
2019-04-13 12:26:25 +0200

    add an autofix when it is a regression, crash or assertion (#543)
    * add an autofix when it is a regression, crash or assertion
    
    * update of the desc

--- auto_nag/scripts/regression_but_type_enhancement_task.py
+++ auto_nag/scripts/regression_but_type_enhancement_task.py
@@ -10,16 +10,23 @@ class RegressionButEnhancementTask(BzCleaner):
         super(RegressionButEnhancementTask, self).__init__()
 
     def description(self):
-        return 'Enhancement or task with the "regression" keyword'
+        return 'Enhancement or task with the "regression", "crash" or "assertion" keyword'
 
     def get_bz_params(self, date):
+        days_lookup = self.get_config('days_lookup')
         params = {
-            "resolution": "---",
-            "keywords": "regression",
-            "keywords_type": "allwords",
+            "resolution": ["---", "FIXED"],
+            "keywords": ["regression", "crash", "assertion"],
+            "keywords_type": "anywords",
             "bug_type": ["task", "enhancement"],
+            'f1': 'days_elapsed',
+            'o1': 'lessthan',
+            'v1': days_lookup,
         }
         return params
+
+    def get_autofix_change(self):
+        return {"type": "defect"}
 
 
 if __name__ == "__main__":
