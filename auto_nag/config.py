6de821e0e1c706ac3a53c72eb2ff9579432aaca1
Calixte Denizet <calixte.denizet@gmail.com>
2019-04-22 16:06:12 +0200

    Fix path issue in test

--- auto_nag/config.py
+++ auto_nag/config.py
@@ -2,17 +2,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from libmozdata import config
 import json
+from libmozdata import config
+import os
 
 
 class MyConfig(config.Config):
+
+    PATH = 'auto_nag/scripts/configs/config.json'
+
     def __init__(self):
         super(MyConfig, self).__init__()
-        with open('auto_nag/scripts/configs/config.json') as In:
-            self.conf = json.load(In)
-            if not self.conf.get('bz_api_key', None):
-                raise Exception('Your config.json file must contain a Bugzilla token')
+        if not os.path.exists(MyConfig.PATH):
+            self.conf = {'bz_api_key': ''}
+        else:
+            with open(MyConfig.PATH) as In:
+                self.conf = json.load(In)
+                if not self.conf.get('bz_api_key', None):
+                    raise Exception(
+                        'Your config.json file must contain a Bugzilla token'
+                    )
 
     def get(self, section, option, default=None, type=str):
         if section == 'Bugzilla':
