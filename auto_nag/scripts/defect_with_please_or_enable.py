da87a17631ef19704e6e49ba6120f4adfd49fd3c
Sylvestre Ledru <sledru@mozilla.com>
2019-04-22 16:17:55 +0200

    also add disable to the search (#594)

--- auto_nag/scripts/defect_with_please_or_enable.py
+++ auto_nag/scripts/defect_with_please_or_enable.py
@@ -10,7 +10,7 @@ class DefectWithPlease(BzCleaner):
         super(DefectWithPlease, self).__init__()
 
     def description(self):
-        return "Defect with description starting with 'Please' or 'Enable'"
+        return "Defect with description starting with 'Please', 'Enable' or 'Disable'"
 
     def ignore_date(self):
         return True
@@ -19,7 +19,7 @@ class DefectWithPlease(BzCleaner):
         days_lookup = self.get_config("days_lookup")
         params = {
             "resolution": "---",
-            "short_desc": r"^(please|enable)",
+            "short_desc": r"^(please|enable|disable)",
             "short_desc_type": "regexp",
             "bug_type": ["defect"],
             "f1": "days_elapsed",
