efaf6bce05800ea58954b663e590f216d56f1fe5
Frederik Braun <fbraun+gh@mozilla.com>
2020-01-22 15:32:36 +0100

    Add survey sec bugs (#872)
    * initial attempt
    
    * fixes from calixte
    
    * fixes from calixte / adding template
    
    * fix email, fix whiteboard
    
    * activate shell script (disable dry run)
    
    * reformat
    
    * codespell
    
    * address nits

---
+++ run_survey_sec_bugs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+export PYTHONPATH=.
+
+./runauto_nag_common.sh
+
+. venv/bin/activate
+
+# force the update of dependencies
+pip install -r requirements.txt
+
+# Clean the log files
+python -m auto_nag.log --clean
+
+# Close inactive intermittent bugs
+python -m auto_nag.scripts.survey_sec_bugs
+
+# Send a mail if the logs are not empty
+# MUST ALWAYS BE THE LAST COMMAND
+python -m auto_nag.log --send
+
+deactivate
