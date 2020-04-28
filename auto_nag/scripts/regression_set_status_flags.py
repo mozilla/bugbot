a97391c96d5906ff3972008c475f247f26d6acde
Julien Cristau <jcristau@mozilla.com>
2020-04-28 12:24:53 +0200

    Add license header

--- auto_nag/scripts/regression_set_status_flags.py
+++ auto_nag/scripts/regression_set_status_flags.py
@@ -1,4 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from libmozdata.bugzilla import Bugzilla
+
 from auto_nag import utils
 from auto_nag.bzcleaner import BzCleaner
 
