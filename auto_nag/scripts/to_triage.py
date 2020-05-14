mozilla/bugbot

commit 30aefab615797f5c48f6bba41eb4eef3031883d6

AutoNag changes for Severity triage (#953)
* AutoNag changes for Severity triage

* order parameters for readablity

* Update queries for May 8 change

* Update templates for triage change

* Look for old default Severity

* Fix untriaged message to match defintion

auto_nag/scripts/to_triage.py
@@ -73,13 +73,22 @@ class ToTriage(BzCleaner, Nag):
             "include_fields": fields,
             "product": list(prods),
             "component": list(comps),
+            "email1": "intermittent-bug-filer@mozilla.bugs",
+            "emailreporter1": "1",
+            "emailtype1": "notequals",
+            "email2": "wptsync@mozilla.bugs",
+            "emailreporter2": "1",
+            "emailtype2": "notequals",
             "resolution": "---",
-            "f1": "priority",
+            "f1": "bug_type",
             "o1": "equals",
-            "v1": "--",
+            "v1": "defect",
             "f2": "flagtypes.name",
-            "o2": "notequals",
+            "o2": "notsubstring",
             "v2": "needinfo?",
+            "f3": "bug_severity",
+            "o3": "anyexact",
+            "v3": "--, n/a, normal"
         }
 
         return params

auto_nag/scripts/workflow/no_severity.py
@@ -11,9 +11,9 @@ from auto_nag.nag_me import Nag
 from auto_nag.round_robin import RoundRobin
 
 
-class NoPriority(BzCleaner, Nag):
+class NoSeverity(BzCleaner, Nag):
     def __init__(self, typ):
-        super(NoPriority, self).__init__()
+        super(NoSeverity, self).__init__()
         assert typ in {"first", "second"}
         self.typ = typ
         self.lookup_first = utils.get_config(self.name(), "first-step", 2)
@@ -27,14 +27,14 @@ class NoPriority(BzCleaner, Nag):
         self.components_skiplist = utils.get_config("workflow", "components_skiplist")
 
     def description(self):
-        return "Bugs without a priority set"
+        return "Bugs without a severity or statuses set"
 
     def nag_template(self):
         return self.template()
 
     def nag_preamble(self):
         return """<p>
-  <b>You're getting this email because some bugs for which you're the triage owner or the triage owner deputy have no priority set.</b>
+  <b>You're getting this email because some bugs for which you're the triage owner or the triage owner deputy have no severity set.</b>
   <ul>
     <li><a href="https://firefox-source-docs.mozilla.org/bug-mgmt/policies/triage-bugzilla.html#why-triage">Why triage?</a></li>
     <li><a href="https://firefox-source-docs.mozilla.org/bug-mgmt/policies/triage-bugzilla.html#what-do-you-triage">What do you triage?</a></li>
@@ -97,12 +97,23 @@ class NoPriority(BzCleaner, Nag):
     def get_bz_params(self, date):
         fields = ["triage_owner", "flags"]
         params = {
-            "bug_type": "defect",
             "include_fields": fields,
+            "email1": "intermittent-bug-filer@mozilla.bugs",
+            "emailreporter1": "1",
+            "emailtype1": "notequals",
+            "email2": "wptsync@mozilla.bugs",
+            "emailreporter2": "1",
+            "emailtype2": "notequals",
             "resolution": "---",
-            "f1": "priority",
-            "o1": "equals",
-            "v1": "--",
+            "f21": "bug_type",
+            "o21": "equals",
+            "v21": "defect",
+            "f22": "flagtypes.name",
+            "o22": "notsubstring",
+            "v22": "needinfo?",
+            "f23": "bug_severity",
+            "o23": "anyexact",
+            "v23": "--, n/a, normal"
         }
         self.date = lmdutils.get_date_ymd(date)
         first = f"-{self.lookup_first * 7}d"
@@ -213,5 +224,5 @@ class NoPriority(BzCleaner, Nag):
 
 
 if __name__ == "__main__":
-    NoPriority("first").run()
-    NoPriority("second").run()
+    NoSeverity("first").run()
+    NoSeverity("second").run()

auto_nag/history.py
@@ -192,8 +192,8 @@ class History(object):
                         in c
                     ):
                         tool = "mismatch_priority_tracking_release"
-                    elif c.startswith("The priority flag is not set for this bug.\n:"):
-                        tool = "no_priority"
+                    elif c.startswith("The severity flag is not set for this bug.\n:"):
+                        tool = "no_severity"
                     elif c.startswith(
                         "The priority flag is not set for this bug and there is no activity for"
                     ):

auto_nag/scripts/untriage_important_sev.py
@@ -15,7 +15,7 @@ class UntriagedWithImportantSev(BzCleaner):
     def get_bz_params(self, date):
         return {
             "resolution": ["---"],
-            "bug_severity": ["blocker", "critical", "major"],
+            "bug_severity": ["S1", "S2"],
             "component": "Untriaged",
         }
 

auto_nag/scripts/workflow/multi_nag.py
@@ -5,7 +5,7 @@
 from auto_nag.erroneous_bzmail import check_erroneous_bzmail
 from auto_nag.multinaggers import MultiNaggers
 
-from .no_priority import NoPriority
+from .no_severity import NoSeverity
 
 # from .p1_no_activity import P1NoActivity
 # from .p1_no_assignee import P1NoAssignee
@@ -16,8 +16,8 @@ from .no_priority import NoPriority
 class WorkflowMultiNag(MultiNaggers):
     def __init__(self):
         super(WorkflowMultiNag, self).__init__(
-            NoPriority("first"),
-            NoPriority("second"),
+            NoSeverity("first"),
+            NoSeverity("second"),
             # P1NoActivity(),
             # P1NoAssignee(),
             # P2NoActivity(),

templates/no_severity.html
@@ -1,6 +1,6 @@
 {{ nag_preamble }}
 <p>
-  The following {{ plural('bug has', data, pword='bugs have') }} no priority flag set for the last {{ extra['nweeks'] }} {{ plural('week', extra['nweeks']) }}:
+  The following {{ plural('bug has', data, pword='bugs have') }} no Severity field set for the last {{ extra['nweeks'] }} {{ plural('week', extra['nweeks']) }}:
 </p>
 <table {{ table_attrs }}>
   <thead>

templates/to_triage.html
@@ -1,4 +1,4 @@
-<p>The following {{ plural('bug has', data, pword='bugs have') }} no priority flag set and no pending needinfos (see the triage <a href="https://firefox-source-docs.mozilla.org/bug-mgmt/policies/triage-bugzilla.html#what-do-you-triage">documentation</a>):
+<p>The following {{ plural('bug has', data, pword='bugs have') }} no Severity field set and no pending needinfos (see the triage <a href="https://firefox-source-docs.mozilla.org/bug-mgmt/policies/triage-bugzilla.html#what-do-you-triage">documentation</a>):
 </p>
 <table {{ table_attrs }}>
   <thead>

commit 340d81eadedb187c0d7d070180c7917e17dc9a91

Fix some black formatting issues introduced by 30aefab615797f5c48f6bba41eb4eef3031883d6

auto_nag/scripts/to_triage.py
@@ -88,7 +88,7 @@ class ToTriage(BzCleaner, Nag):
             "v2": "needinfo?",
             "f3": "bug_severity",
             "o3": "anyexact",
-            "v3": "--, n/a, normal"
+            "v3": "--, n/a, normal",
         }
 
         return params

auto_nag/scripts/workflow/no_severity.py
@@ -113,7 +113,7 @@ class NoSeverity(BzCleaner, Nag):
             "v22": "needinfo?",
             "f23": "bug_severity",
             "o23": "anyexact",
-            "v23": "--, n/a, normal"
+            "v23": "--, n/a, normal",
         }
         self.date = lmdutils.get_date_ymd(date)
         first = f"-{self.lookup_first * 7}d"