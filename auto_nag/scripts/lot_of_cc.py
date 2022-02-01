4ac56ae2f84b423b04487b5b61af8d435d3fd37d
Sylvestre Ledru <sledru@mozilla.com>
2022-02-01 11:04:48 +0100

    surface bugs with a lot of cc & votes (#1280)
    * surface bugs with a lot of cc & votes
    
    * remove artifact
    
    * fix black

--- auto_nag/scripts/lot_of_cc.py
+++ auto_nag/scripts/lot_of_cc.py
@@ -6,14 +6,14 @@ from auto_nag import utils
 from auto_nag.bzcleaner import BzCleaner
 
 
-class SeveralDups(BzCleaner):
+class SeveralCc(BzCleaner):
     def __init__(self):
-        super(SeveralDups, self).__init__()
+        super(SeveralCc, self).__init__()
         self.nweeks = utils.get_config(self.name(), "weeks_lookup")
-        self.ndups = utils.get_config(self.name(), "number_dups")
+        self.cc = utils.get_config(self.name(), "number_cc")
 
     def description(self):
-        return "Bugs with several dups for the last {} weeks".format(self.nweeks)
+        return "Bugs with several cc for the last {} weeks".format(self.nweeks)
 
     def columns(self):
         return ["id", "summary", "creation", "last_change"]
@@ -33,9 +33,9 @@ class SeveralDups(BzCleaner):
             "f1": "days_elapsed",
             "o1": "lessthan",
             "v1": self.nweeks * 7,
-            "f2": "dupe_count",
+            "f2": "cc_count",
             "o2": "greaterthaneq",
-            "v2": self.ndups,
+            "v2": self.cc,
             "f3": "keywords",
             "o3": "nowords",
             "v3": ["meta", "intermittent"],
@@ -44,4 +44,4 @@ class SeveralDups(BzCleaner):
 
 
 if __name__ == "__main__":
-    SeveralDups().run()
+    SeveralCc().run()
