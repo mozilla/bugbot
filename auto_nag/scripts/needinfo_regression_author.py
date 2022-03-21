2ddfa8349ea3bedf0c3cf5feea1225dabf0108ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-03-21 18:19:54 +0100

    Avoid trying to needinfo nobody in the needinfo_regression_author script
    Fixes #1332

--- auto_nag/scripts/needinfo_regression_author.py
+++ auto_nag/scripts/needinfo_regression_author.py
@@ -6,7 +6,7 @@ import collections
 
 from libmozdata.bugzilla import Bugzilla
 
-from auto_nag import utils
+from auto_nag import logger, utils
 from auto_nag.bzcleaner import BzCleaner
 
 
@@ -97,6 +97,16 @@ class RegressionSetStatusFlags(BzCleaner):
         ).get_data().wait()
 
     def filter_bugs(self, bugs):
+        # Exclude bugs whose regressor author is nobody.
+        for bug in list(bugs.values()):
+            if utils.is_no_assignee(bug["regressor_author_email"]):
+                logger.warning(
+                    "Bug {}, regressor of bug {}, doesn't have an author".format(
+                        bug["regressor_id"], bug["id"]
+                    )
+                )
+                del bugs[bug["id"]]
+
         # Exclude bugs whose creator is the regressor author.
         bugs = {
             bug["id"]: bug
