932470281100681542270a27981a637db8e500f2
Tom Ritter <tom@ritter.vg>
2022-04-06 12:55:33 -0400

    Disable the sec survey for now (#1386)

--- run_survey_sec_bugs.sh
+++ run_survey_sec_bugs.sh
@@ -3,21 +3,21 @@ set -e
 
 export PYTHONPATH=.
 
-./runauto_nag_common.sh
+#./runauto_nag_common.sh
 
-. venv/bin/activate
+#. venv/bin/activate
 
 # force the update of dependencies
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Clean the log files
-python -m auto_nag.log --clean
+#python -m auto_nag.log --clean
 
 # Close inactive intermittent bugs
-python -m auto_nag.scripts.survey_sec_bugs --production
+#python -m auto_nag.scripts.survey_sec_bugs --production
 
 # Send a mail if the logs are not empty
 # MUST ALWAYS BE THE LAST COMMAND
-python -m auto_nag.log --send
+#python -m auto_nag.log --send
 
-deactivate
+#deactivate
