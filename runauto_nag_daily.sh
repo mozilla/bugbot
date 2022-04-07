8f877f463212f70d01c75da7f20f52ec7b805c02
Suhaib Mujahid <smujahid@mozilla.com>
2022-04-07 19:11:31 -0400

    Run the tool daily

--- runauto_nag_daily.sh
+++ runauto_nag_daily.sh
@@ -153,6 +153,9 @@ python -m auto_nag.scripts.severity_high_security --production
 # Nag for components that need triage owner to be assigned
 python -m auto_nag.scripts.vacant_triage_owner --production
 
+# Notify about bugs with needinfo requests on inactive users
+python -m auto_nag.scripts.inactive_ni_pending --production
+
 # Send a mail if the logs are not empty
 # MUST ALWAYS BE THE LAST COMMAND
 python -m auto_nag.log --send
