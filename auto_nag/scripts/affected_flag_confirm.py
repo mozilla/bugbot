53c3f694b2e7cb88c0f58d2e031dcac08e6d9457
Suhaib Mujahid <smujahid@mozilla.com>
2022-04-12 16:47:53 -0400

    Revert "Drop checking the history"
    This reverts commit 0b0f1c19889a88a4b5fab928e590b1580d62834a.

--- auto_nag/scripts/affected_flag_confirm.py
+++ auto_nag/scripts/affected_flag_confirm.py
@@ -2,16 +2,66 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
+from libmozdata.bugzilla import Bugzilla
+
 from auto_nag import utils
 from auto_nag.bzcleaner import BzCleaner
+
+VERSION_PREFIX = "cf_status_firefox"
+N = len(VERSION_PREFIX)
 
 
 class AffectedFlagConfirm(BzCleaner):
     def description(self):
         return "Unconfirmed bugs that have an affected Firefox version flag"
 
+    def handle_bug(self, bug, data):
+        bugid = str(bug["id"])
+        data[bugid] = {"creator": bug["creator"]}
+        return bug
+
+    def get_bugs(self, *args, **kwargs):
+        bugs = super().get_bugs(*args, **kwargs)
+        self.query_url = None
+
+        def history_handler(bug, data):
+            bugid = str(bug["id"])
+            creator = data[bugid]["creator"]
+            removed_status = set()
+            for event in reversed(bug["history"]):
+                for change in event["changes"]:
+                    if (
+                        change["field_name"].startswith(VERSION_PREFIX)
+                        and change["field_name"] not in removed_status
+                    ):
+                        if change["added"] == "affected":
+                            if event["who"] != creator:
+                                affected_version = True
+                                break
+                        elif change["removed"] == "affected":
+                            removed_status.add(change["field_name"])
+                else:
+                    affected_version = False
+
+                if affected_version:
+                    break
+            else:
+                del data[bugid]
+
+        Bugzilla(
+            list(bugs.keys()),
+            historyhandler=history_handler,
+            historydata=bugs,
+            timeout=960,
+        ).get_data().wait()
+
+        return bugs
+
     def get_bz_params(self, date):
+        fields = ["creator"]
         params = {
+            "include_fields": fields,
             "bug_status": "UNCONFIRMED",
             "j1": "OR",
             "f1": "OP",
