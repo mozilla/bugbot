d6855674bf2ee688103ac686588025e1d199e04f
Suhaib Mujahid <smujahid@mozilla.com>
2022-04-30 21:01:47 -0400

    Perform actions as suggested by Marco

--- auto_nag/scripts/inactive_ni_pending.py
+++ auto_nag/scripts/inactive_ni_pending.py
@@ -2,16 +2,28 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from dateutil.relativedelta import relativedelta
 from libmozdata import utils as lmdutils
 
 from auto_nag import utils
 from auto_nag.bzcleaner import BzCleaner
 from auto_nag.user_activity import UserActivity
 
+# TODO: should be moved when resolving https://github.com/mozilla/relman-auto-nag/issues/1384
+HIGH_PRIORITY = {"P1", "P2"}
+HIGH_SEVERITY = {"S1", "critical", "S2", "major"}
+
 
 class InactiveNeedinfoPending(BzCleaner):
     def __init__(self):
         super(InactiveNeedinfoPending, self).__init__()
+        self.max_actions = utils.get_config(self.name(), "max_actions", 7)
+        self.__recent_bug_limit = lmdutils.get_date_ymd("today") - relativedelta(
+            months=6
+        )
+
+    def get_max_actions(self):
+        return self.max_actions
 
     def description(self):
         return "Bugs with needinfo pending on inactive people"
@@ -64,20 +76,88 @@ class InactiveNeedinfoPending(BzCleaner):
                 if flag["requestee"] in inactive_users
             ]
             bug["inactive_ni_count"] = len(bug["inactive_ni"])
+            self.add_action(bug)
+
+    def is_recent_bug(self, bug):
+        return lmdutils.get_date_ymd(bug["creation_time"]) >= self.__recent_bug_limit
+
+    def add_action(self, bug):
+        users_num = len(set([flag["requestee"] for flag in bug["inactive_ni"]]))
+
+        if (
+            (
+                bug["priority"] in HIGH_PRIORITY
+                or bug["severity"] in HIGH_SEVERITY
+                or self.is_recent_bug(bug)
+            )
+            and not utils.is_no_assignee(bug["triage_owner"])
+            and not any(
+                [
+                    flag["requestee"] == bug["triage_owner"]
+                    for flag in bug["inactive_ni"]
+                ]
+            )
+        ):
+            autofix = {
+                "flags": [
+                    {
+                        "id": flag["id"],
+                        "requestee": bug["triage_owner"],
+                    }
+                    for flag in bug["inactive_ni"]
+                ],
+                "comment": {
+                    "body": f"Redirect needinfo pending on inactive {utils.plural('user', users_num)} to the triage owner.",
+                },
+            }
+        else:
+            autofix = {
+                "flags": [
+                    {
+                        "id": flag["id"],
+                        "status": "X",
+                    }
+                    for flag in bug["inactive_ni"]
+                ],
+                "comment": {
+                    "body": f"Clear needinfo pending on inactive {utils.plural('user', users_num)}.",
+                },
+            }
+
+        self.add_prioritized_action(bug, bug["triage_owner"], autofix=autofix)
+
+    def get_bug_sort_key(self, bug):
+        return (
+            bug["priority"] not in HIGH_PRIORITY,
+            bug["severity"] not in HIGH_SEVERITY,
+            not self.is_recent_bug(bug),
+            utils.get_sort_by_bug_importance_key(bug),
+        )
 
     def handle_bug(self, bug, data):
         bugid = str(bug["id"])
         data[bugid] = {
+            "priority": bug["priority"],
+            "severity": bug["severity"],
+            "creation_time": bug["creation_time"],
+            "triage_owner": bug["triage_owner"],
             "needinfo_flags": [
                 flag for flag in bug["flags"] if flag["name"] == "needinfo"
-            ]
+            ],
         }
 
         return bug
 
     def get_bz_params(self, date):
-        fields = ["flags"]
         date = lmdutils.get_date_ymd(date)
+
+        fields = [
+            "triage_owner",
+            "flags",
+            "priority",
+            "severity",
+            "creation_time",
+        ]
 
         params = {
             "include_fields": fields,
