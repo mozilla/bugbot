f07232adb829f9b642c2e44cadd6ca9946006a5a
Suhaib Mujahid <suhaibmujahid@gmail.com>
2022-06-08 11:12:30 -0400

    Set the missing beta status only when nightly and release are affected (#1463)
    Co-authored-by: Marco Castelluccio <mcastelluccio@mozilla.com>

--- auto_nag/scripts/missing_beta_status.py
+++ auto_nag/scripts/missing_beta_status.py
@@ -13,7 +13,7 @@ class MissingBetaStatus(BzCleaner):
         if not self.init_versions():
             return
 
-        self.status_central = utils.get_flag(
+        self.status_nightly = utils.get_flag(
             self.versions["central"], "status", "nightly"
         )
         self.status_beta = utils.get_flag(self.versions["beta"], "status", "beta")
@@ -30,39 +30,30 @@ class MissingBetaStatus(BzCleaner):
     def get_autofix_change(self):
         return self.autofix_status
 
+    @staticmethod
+    def _is_affected(status):
+        return status and status not in ("unaffected", "?")
+
     def handle_bug(self, bug, data):
         bugid = str(bug["id"])
-        central = bug[self.status_central]
+        nightly = bug[self.status_nightly]
         release = bug[self.status_release]
         doc = self.get_documentation()
 
-        if central == release:
-            if release != "verified":
-                self.autofix_status[bugid] = {
-                    "comment": {
-                        "body": "Change the status for beta to have the same as nightly and release.\n{}".format(
-                            doc
-                        )
-                    },
-                    self.status_beta: central,
-                }
-            else:
-                # if the two status are different, we don't know what to set
-                # if this verified on nightly and release, we cannot say
-                # per say if this is verified
-                self.autofix_status[bugid] = {
-                    "comment": {
-                        "body": "Since it has been verified for nightly and release, is it verified for beta too?\n{}".format(
-                            doc
-                        )
-                    },
-                    self.status_beta: "?",
-                }
+        if self._is_affected(nightly) and self._is_affected(release):
+            self.autofix_status[bugid] = {
+                "comment": {
+                    "body": f"Since nightly and release are affected, beta will likely be affected too.\n${doc}"
+                },
+                self.status_beta: "affected",
+            }
         else:
             self.autofix_status[bugid] = {
                 "comment": {
-                    "body": "Since the status are different for nightly and release, what's the status for beta?\n{}".format(
-                        doc
+                    "body": (
+                        f"Since the status is marked as `${nightly}` for nightly and release, is it `${nightly}` for beta too?\n${doc}"
+                        if nightly == release
+                        else f"Since the status is marked as `${nightly}` for nightly and as `${release}` for release, is it `${nightly}` or `${release}` for beta?\n${doc}"
                     )
                 },
                 self.status_beta: "?",
@@ -70,7 +61,7 @@ class MissingBetaStatus(BzCleaner):
         return bug
 
     def get_bz_params(self, date):
-        fields = [self.status_central, self.status_release]
+        fields = [self.status_nightly, self.status_release]
         params = {
             "include_fields": fields,
             "f1": self.status_beta,
@@ -79,7 +70,7 @@ class MissingBetaStatus(BzCleaner):
             "f2": self.status_release,
             "o2": "notequals",
             "v2": "---",
-            "f3": self.status_central,
+            "f3": self.status_nightly,
             "o3": "notequals",
             "v3": "---",
         }
