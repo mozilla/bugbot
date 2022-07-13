703aee192612d8d90e8fea45d5023209bfde55cb
Suhaib Mujahid <smujahid@mozilla.com>
2022-07-13 13:25:15 -0400

    Enable the triage owner rotations tool

--- runauto_nag_daily.sh
+++ runauto_nag_daily.sh
@@ -16,6 +16,9 @@ python -m auto_nag.log --clean
 # Daily
 python -m auto_nag.next_release --production
 
+# Update the triage owners on Bugzilla
+python -m auto_nag.scripts.triage_owner_rotations --production
+
 # Send a todo list to set priority
 # Daily
 python -m auto_nag.scripts.to_triage --production
