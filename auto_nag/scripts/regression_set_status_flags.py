b00ecf3e7684904ed6752b91eda8847ed9ce0bf2
Suhaib Mujahid <smujahid@mozilla.com>
2022-09-02 09:22:13 -0400

    [regression_set_status_flags] Fix setting the `regression` keyword

--- auto_nag/scripts/regression_set_status_flags.py
+++ auto_nag/scripts/regression_set_status_flags.py
@@ -168,12 +168,12 @@ class RegressionSetStatusFlags(BzCleaner):
             regressor = bugs[bugid]["regressed_by"]
             self.status_changes[bugid]["comment"] = {
                 "body": f"{self.description()} {regressor}",
-                "keywords": {"add": ["regression"]},
                 # if the regressing bug is private (security or otherwise
                 # confidential), don't leak its number through our comment (the
                 # regressed_by field is not public in that case)
                 "is_private": bool(data[regressor].get("groups")),
             }
+            self.status_changes[bugid]["keywords"] = {"add": ["regression"]}
 
         return filtered_bugs
 
