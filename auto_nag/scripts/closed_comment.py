8111eb9cc1837e4b815d467706695f4e687ffca6
Suhaib Mujahid <smujahid@mozilla.com>
2022-10-14 23:36:52 -0400

    Improve the filtering and customize the comments for duplicate bugs

--- auto_nag/scripts/closed_comment.py
+++ auto_nag/scripts/closed_comment.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from libmozdata.bugzilla import Bugzilla
+
 from auto_nag import utils
 from auto_nag.bzcleaner import BzCleaner
 from auto_nag.people import People
@@ -17,17 +19,32 @@ class ClosedComment(BzCleaner):
 
     def handle_bug(self, bug, data):
         last_comment = bug["comments"][-1]
+        # Ignore a comment if:
+        #   - has any tag (e.g., spam)
+        #   - was posted before closing the bug
+        #   - was posted by:
+        #       - the bug creator
+        #       - the bug assignee
+        #       - an internal user (e.g., a bot or employee)
+        #       - a participant who previously commented on the bug
         if (
-            last_comment["creation_time"] <= bug["cf_last_resolved"]
+            last_comment["tags"]
+            or last_comment["creation_time"] <= bug["cf_last_resolved"]
             or last_comment["creator"] in (bug["creator"], bug["assigned_to"])
             or self._internal_user(last_comment["creator"])
             or any(
                 last_comment["creator"] == comment["creator"]
-                and comment["creation_time"] < bug["cf_last_resolved"]
+                and comment["creation_time"] <= bug["cf_last_resolved"]
                 for comment in bug["comments"]
             )
         ):
             return None
+
+        bugid = str(bug["id"])
+        data[bugid] = {
+            "dupe_of": bug["dupe_of"],
+            "is_incomplete": bug["resolution"] == "INCOMPLETE",
+        }
 
         return bug
 
@@ -39,34 +56,76 @@ class ClosedComment(BzCleaner):
             or self.people.is_mozilla(email)
         )
 
+    def get_bugs(self, date="today", bug_ids=..., chunk_size=None):
+        bugs = super().get_bugs(date, bug_ids, chunk_size)
+
+        dup_bug_ids = {bug["dupe_of"] for bug in bugs.values() if bug["dupe_of"]}
+        dup_bug_is_open = {}
+        if dup_bug_ids:
+            Bugzilla(
+                dup_bug_ids,
+                include_fields=["id", "is_open"],
+                bughandler=self._handle_dup_bug,
+                bugdata=dup_bug_is_open,
+            ).wait()
+
+        for bug_id, bug in list(bugs.items()):
+            if dup_bug_is_open.get(bug["dupe_of"]):
+                dupe_of_id = bug["dupe_of"]
+                self.autofix_changes[bug_id] = {
+                    "comment": {
+                        "body": f"This bug is a duplicate of bug {dupe_of_id}. If you have an input, please comment in bug {dupe_of_id} instead."
+                    },
+                }
+            elif not bug["is_incomplete"]:
+                self.autofix_changes[bug_id] = {
+                    "comment": {
+                        "body": {"body": "This bug is closed. Please file a new bug."}
+                    },
+                }
+
+        return bugs
+
+    def _handle_dup_bug(self, bug, data):
+        data[bug["id"]] = bug["is_open"]
+
+    def ignore_meta(self):
+        return True
+
+    def has_access_to_sec_bugs(self):
+        return False
+
     def get_bz_params(self, date):
         fields = [
+            "resolution",
+            "dupe_of",
             "assigned_to",
             "creator",
             "cf_last_resolved",
             "comments.creator",
             "comments.creation_time",
+            "comments.tags",
         ]
 
         params = {
             "include_fields": fields,
             "bug_type": "defect",
             "bug_status": "__closed__",
+            "resolution_type": "notequals",
+            "resolution": "WONTFIX",
             "f1": "cf_last_resolved",
-            "o1": "greaterthan",
+            "o1": "lessthan",
             "v1": "-5d",
             "n2": 1,
             "f2": "longdesc",
             "o2": "casesubstring",
             "v2": "Please file a new bug",
+            "f3": "longdesc",
+            "o3": "changedafter",
+            "v3": "-4d",
         }
 
         return params
-
-    def get_autofix_change(self):
-        return {
-            "comment": {"body": "This bug is closed. Please file a new bug."},
-        }
 
 
 if __name__ == "__main__":
