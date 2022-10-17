86a118b4b176e9c2eeaca5727c07e37fee631338
Suhaib Mujahid <smujahid@mozilla.com>
2022-10-17 18:04:43 -0400

    [severity_underestimated] Disable the tool temporary

--- runauto_nag_weekdays.sh
+++ runauto_nag_weekdays.sh
@@ -149,7 +149,7 @@ python -m auto_nag.scripts.assignee_no_login --production
 python -m auto_nag.scripts.severity_inconsistency --production
 
 # Needinfo for bugs with underestimated severity levels
-python -m auto_nag.scripts.severity_underestimated --production
+# python -m auto_nag.scripts.severity_underestimated --production
 
 # Needinfo for bugs with high security keywords whose set to low severity
 python -m auto_nag.scripts.severity_high_security --production
