fd05ca38d9cae07e68f6769a03bdd24698a9d92d
Suhaib Mujahid <smujahid@mozilla.com>
2022-11-23 08:09:08 -0500

    [duplicate_copy_metadata] Copy the `Regressed by` field (#1779)
    * Copy only regressors that were created before the regression
    
    * Add the `regression` keyword automatically

--- auto_nag/scripts/duplicate_copy_metadata.py
+++ auto_nag/scripts/duplicate_copy_metadata.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from typing import List, Set
+from typing import Any, Dict, List, Set
 
 from libmozdata.bugzilla import Bugzilla
 
@@ -15,6 +15,7 @@ FIELD_NAME_TO_LABEL = {
     "severity": "Severity",
     "whiteboard": "Whiteboard",
     "cf_performance_impact": "Performance Impact",
+    "regressed_by": "Regressed by",
 }
 
 FIELD_LABEL_TO_NAME = {label: name for name, label in FIELD_NAME_TO_LABEL.items()}
@@ -46,13 +47,13 @@ class DuplicateCopyMetadata(BzCleaner):
                 "duplicates",
                 "cf_performance_impact",
                 "comments",
+                "history",
+                "regressed_by",
                 "is_open",
             ],
             bughandler=self.handle_bug,
             bugdata=original_bugs,
         ).wait()
-
-        assert len(original_bug_ids) == len(original_bugs)
 
         results = {}
         for bug_id, bug in original_bugs.items():
@@ -61,9 +62,13 @@ class DuplicateCopyMetadata(BzCleaner):
 
             copied_fields = {}
             for dup_bug_id in bug["duplicates"]:
-                dup_bug = dup_bugs.get(str(dup_bug_id))
+                dup_bug_id = str(dup_bug_id)
+                dup_bug = dup_bugs.get(dup_bug_id)
                 if not dup_bug:
                     continue
+
+                # TODO: Since the logic for copied fields is getting bigger,
+                # consider refactoring it in a separate method.
 
                 # Performance Impact: copy the assessment result from duplicates
                 if bug.get("cf_performance_impact") == "---" and dup_bug.get(
@@ -109,14 +114,34 @@ class DuplicateCopyMetadata(BzCleaner):
                     elif new_access_tag == copied_fields["whiteboard"]["value"]:
                         copied_fields["whiteboard"]["from"].append(dup_bug["id"])
 
+                # Regressed by: move the regressed_by field to the duplicate of
+                if dup_bug["regressed_by"]:
+                    added_regressed_by = self.get_previously_added_regressors(bug)
+                    new_regressed_by = {
+                        regression_bug_id
+                        for regression_bug_id in dup_bug["regressed_by"]
+                        if regression_bug_id not in added_regressed_by
+                        and regression_bug_id < int(bug_id)
+                    }
+                    if new_regressed_by:
+                        if "regressed_by" not in copied_fields:
+                            copied_fields["regressed_by"] = {
+                                "from": [dup_bug["id"]],
+                                "value": new_regressed_by,
+                            }
+                        else:
+                            copied_fields["regressed_by"]["from"].append(dup_bug["id"])
+                            copied_fields["regressed_by"]["value"] |= new_regressed_by
+
             previously_copied_fields = self.get_previously_copied_fields(bug)
+            # We do not need to ignore the `regressed_by` field because we
+            # already check the history to avoid overwriting the engineers.
+            previously_copied_fields.discard("regressed_by")
             copied_fields = sorted(
                 (
                     field,
                     change["value"],
-                    utils.english_list(
-                        sorted(f"bug {bug_id}" for bug_id in change["from"])
-                    ),
+                    change["from"],
                 )
                 for field, change in copied_fields.items()
                 if field not in previously_copied_fields
@@ -142,9 +167,9 @@ class DuplicateCopyMetadata(BzCleaner):
                 bugs they were copied from (field, value, source).
         """
         bug_id = str(bug["id"])
-        autofix = {}
+        autofix: Dict[str, Any] = {}
 
-        duplicates = {source for _, _, source in copied_fields}
+        duplicates = {id for _, _, source in copied_fields for id in source}
 
         # NOTE: modifying the following comment template should also be
         # reflected in the `get_previously_copied_fields` method.
@@ -157,18 +182,31 @@ class DuplicateCopyMetadata(BzCleaner):
 
         for field, value, source in copied_fields:
             if field == "keywords":
-                autofix["keywords"] = {"add": value}
+                autofix["keywords"] = {"add": [value]}
             elif field == "whiteboard":
                 autofix["whiteboard"] = bug["whiteboard"] + value
             elif field == "cf_performance_impact":
                 autofix["cf_performance_impact"] = value
+            elif field == "regressed_by":
+                autofix["regressed_by"] = {"add": list(value)}
+                value = utils.english_list(sorted(f"bug {id}" for id in value))
             else:
                 raise ValueError(f"Unsupported field: {field}")
 
-            comment += f"| {FIELD_NAME_TO_LABEL[field]} | {value} | {source} |\n"
+            field_label = FIELD_NAME_TO_LABEL[field]
+            source = utils.english_list(sorted(f"bug {id}" for id in source))
+            comment += f"| {field_label} | {value} | {source} |\n"
 
         comment += "\n\n" + self.get_documentation()
         autofix["comment"] = {"body": comment}
+        # The following is to reduce noise by having the bot to comme later to
+        # add the `regression` keyword.
+        if "regressed_by" in autofix and "regression" not in bug["keywords"]:
+            if "keywords" not in autofix:
+                autofix["keywords"] = {"add": ["regression"]}
+            else:
+                autofix["keywords"]["add"].append("regression")
+
         self.autofix_changes[bug_id] = autofix
 
     def get_previously_copied_fields(self, bug: dict) -> Set[str]:
@@ -203,6 +241,28 @@ class DuplicateCopyMetadata(BzCleaner):
 
         return previously_copied_fields
 
+    def get_previously_added_regressors(self, bug: dict) -> Set[int]:
+        """Get the bug ids for regressors that have been added to a bug in the
+        past.
+
+        Args:
+            bug: The bug to get the previously added regressors for.
+
+        Returns:
+            A set of ids for previously added regressors.
+        """
+        added_regressors = {
+            int(bug_id)
+            for entry in bug["history"]
+            for change in entry["changes"]
+            if change["field_name"] == "regressed_by"
+            for bug_id in change["removed"].split(",")
+            if bug_id
+        }
+        added_regressors.update(bug["regressed_by"])
+
+        return added_regressors
+
     def columns(self):
         return ["id", "summary", "copied_fields"]
 
@@ -212,6 +272,7 @@ class DuplicateCopyMetadata(BzCleaner):
             "keywords",
             "cf_performance_impact",
             "dupe_of",
+            "regressed_by",
         ]
 
         params = {
@@ -223,6 +284,7 @@ class DuplicateCopyMetadata(BzCleaner):
                 "keywords",
                 "status_whiteboard",
                 "cf_performance_impact",
+                "regressed_by",
             ],
         }
 
