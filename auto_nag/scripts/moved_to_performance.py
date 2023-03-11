269e76fd95ca444788ff2ff6cb989af53c534abf
Suhaib Mujahid <smujahid@mozilla.com>
2023-03-10 23:09:01 -0500

    Avoid asking for information that already present in the bug
    Fixes #1926

--- auto_nag/scripts/moved_to_performance.py
+++ auto_nag/scripts/moved_to_performance.py
@@ -2,14 +2,100 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import base64
+import json
+
 from auto_nag.bzcleaner import BzCleaner
 
 
 class MovedToPerformance(BzCleaner):
     """Add a comment to bugs that recently moved to the performance component"""
 
+    def __init__(self):
+        super().__init__()
+        self.ni_extra = {}
+
     def description(self):
         return "Bugs that recently moved to the performance component"
+
+    def get_extra_for_needinfo_template(self):
+        return self.ni_extra
+
+    def handle_bug(self, bug, data):
+        bugid = str(bug["id"])
+
+        has_profiler_link = (
+            any(
+                "https://share.firefox.dev/" in comment["text"]
+                for comment in bug["comments"]
+            )
+            or "https://perf-html.io/from-url/" in bug["url"]
+        )
+
+        has_memory_report = any(
+            self._is_memory_report_file(attachment)
+            for attachment in bug["attachments"]
+            if not attachment["is_obsolete"] and not attachment["is_patch"]
+        )
+
+        has_troubleshooting_info = any(
+            self._is_troubleshooting_content(attachment)
+            for attachment in bug["attachments"]
+            if not attachment["is_obsolete"] and not attachment["is_patch"]
+        )
+
+        if has_profiler_link and has_memory_report and has_troubleshooting_info:
+            # Nothing missing, no need to add a comment
+            return None
+
+        self.ni_extra[bugid] = {
+            "has_profiler_link": has_profiler_link,
+            "has_memory_report": has_memory_report,
+            "has_troubleshooting_info": has_troubleshooting_info,
+        }
+
+        return bug
+
+    @staticmethod
+    def _is_memory_report_file(attachment):
+        return (
+            attachment["content_type"]
+            in (
+                "application/gzip",
+                "application/x-gzip",
+                "application/json",
+            )
+            and "memory-report" in attachment["file_name"]
+        )
+
+    @staticmethod
+    def _is_troubleshooting_content(attachment):
+        if (
+            attachment["content_type"]
+            not in (
+                "application/json",
+                "text/plain",
+            )
+            or "memory-report" in attachment["file_name"]
+        ):
+            return False
+
+        content_bytes = base64.b64decode(attachment["data"])
+        try:
+            content = json.loads(content_bytes)
+        except ValueError:
+            return False
+
+        return all(
+            key in content
+            for key in (
+                "application",
+                "processes",
+                "addons",
+                "startupCache",
+                "modifiedPreferences",
+            )
+        )
 
     def ignore_meta(self):
         return True
@@ -20,6 +106,13 @@ class MovedToPerformance(BzCleaner):
     def get_bz_params(self, date):
         fields = [
             "creator",
+            "attachments.is_obsolete",
+            "attachments.is_patch",
+            "attachments.content_type",
+            "attachments.data",
+            "attachments.file_name",
+            "comments.text",
+            "url",
         ]
 
         params = {
