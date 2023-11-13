9be19904f90ed97e675ccb7cb4847424d2cd64c3
Sylvestre Ledru <sylvestre@debian.org>
2023-11-13 18:12:09 +0100

    add a new rule to notify bugs with a lot of comments

---
+++ bugbot/rules/lot_of_comments.py
@@ -0,0 +1,53 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from bugbot import utils
+from bugbot.bzcleaner import BzCleaner
+
+
+class SeveralComments(BzCleaner):
+    def __init__(self):
+        super(SeveralComments, self).__init__()
+        self.nweeks = utils.get_config(self.name(), "weeks_lookup")
+        self.comments = utils.get_config(self.name(), "number_comments")
+        print("foo")
+
+    def description(self):
+        return "Bugs with several comments for the last {} weeks".format(self.nweeks)
+
+    def columns(self):
+        return ["id", "summary", "creation", "last_change"]
+
+    def handle_bug(self, bug, data):
+        bugid = str(bug["id"])
+        comment_count = bug["comment_count"]
+
+        data[bugid] = {
+            "creation": utils.get_human_lag(bug["creation_time"]),
+            "last_change": utils.get_human_lag(bug["last_change_time"]),
+            "comment_count": comment_count,
+        }
+        return bug
+
+    def get_bz_params(self, date):
+        print("foo")
+        params = {
+            "include_fields": ["creation_time", "last_change_time", "comment_count"],
+            "resolution": "---",
+            "f1": "days_elapsed",
+            "o1": "lessthan",
+            "v1": self.nweeks * 7,
+            "f2": "longdescs.count",
+            "o2": "greaterthaneq",
+            "v2": self.comments,
+            "f3": "keywords",
+            "o3": "nowords",
+            "v3": ["meta", "intermittent"],
+        }
+        print(params)
+        return params
+
+
+if __name__ == "__main__":
+    SeveralComments().run()
