7a14217fd435377fa024d0b405eddd07e465449e
Benjamin Mah <benjaminmah2004@gmail.com>
2024-07-17 16:40:19 -0400

    Reduced the threshold to consider all users inactive to 4 weeks

--- bugbot/rules/inactive_ni_pending.py
+++ bugbot/rules/inactive_ni_pending.py
@@ -74,7 +74,9 @@ class InactiveNeedinfoPending(BzCleaner):
 
                 requestee_bugs[flag["requestee"]].append(bugid)
 
-        user_activity = UserActivity(include_fields=["groups"])
+        user_activity = UserActivity(
+            include_fields=["groups"], activity_weeks_count=4, absent_weeks_count=4
+        )
         needinfo_requestees = set(requestee_bugs.keys())
         triage_owners = {bug["triage_owner"] for bug in bugs.values()}
         inactive_users = user_activity.check_users(
