1c2cdbe83c5988b62d671838339284e47d315022
Gregory Mierzwinski <gmierz1@live.ca>
2024-10-31 17:56:19 -0400

    Fix linting issue.

--- bugbot/rules/perfalert_inactive_regression_nag.py
+++ bugbot/rules/perfalert_inactive_regression_nag.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from libmozdata.bugzilla import Bugzilla
-
 from bugbot import logger, utils
 from bugbot.rules.needinfo_regression_author import NeedinfoRegressionAuthor
 from bugbot.user_activity import UserActivity, UserStatus
