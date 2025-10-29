f210356288e3be7ffdfb0a4ada77f84d2fd203ed
Suhaib Mujahid <suhaibmujahid@gmail.com>
2025-10-29 14:39:56 -0400

    Add logger adapter with bugbot_rule context

--- bugbot/bzcleaner.py
+++ bugbot/bzcleaner.py
@@ -17,6 +17,7 @@ from libmozdata import config
 from libmozdata import utils as lmdutils
 from libmozdata.bugzilla import Bugzilla
 
+import bugbot
 from bugbot import db, logger, mail, utils
 from bugbot.cache import Cache
 from bugbot.nag_me import Nag
@@ -842,6 +843,8 @@ class BzCleaner(object):
 
     def run(self):
         """Run the rule"""
+        bugbot.logger_extra["bugbot_rule"] = self.name()
+
         args = self.get_args_parser().parse_args()
         self.parse_custom_arguments(args)
         date = "" if self.ignore_date() else args.date
